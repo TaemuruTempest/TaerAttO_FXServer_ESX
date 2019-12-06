c6ee381344e49e269c666e5df14c285963813fc9
TaerAttO <50113216+MonsterTaerAttO@users.noreply.github.com>
2019-12-06 14:04:42 +0700

    update

---
+++ server-data/resources/[TaerAttO]/[taeratto_test]/radialmenu/__resource.lua
@@ -0,0 +1,46 @@
+resource_manifest_version "44febabe-d386-4d18-afbe-5e627f4af937"
+
+client_script {
+    "config.lua",
+    "radialmenu.lua"
+}
+
+ui_page "html/menu.html"
+
+files {
+	"html/menu.html",
+	"html/raphael.min.js",
+    "html/wheelnav.min.js",
+    "html/doors.png",
+    "html/engine.png",
+    "html/hood.png",
+    "html/key.png",
+    "html/trunk.png",
+    "html/smartphone.png",
+    "html/backpack.png",
+    "html/remotekey.png",
+    "html/padlock.png",
+    "html/seatbelt.png",
+    "html/barbielyftupp.png",
+    "html/closet.png",
+    "html/jacket.png",
+    "html/clothes.png",
+    "html/sneaker.png",
+    "html/theatre.png",
+    "html/glasses.png",
+    "html/earrings.png",
+    "html/refresh.png",
+    "html/helmet.png",
+    --[[
+    
+    
+    
+    
+    
+    
+    
+    
+    "html/",
+    "html/",
+    "html/",--]]
+}
---
+++ server-data/resources/[TaerAttO]/[taeratto_test]/radialmenu/config.lua
@@ -0,0 +1,169 @@
+-- Menu configuration, array of menus to display
+menuConfigs = {
+    ['emotes'] = {                                  -- Example menu for emotes when player is on foot
+        enableMenu = function()                     -- Function to enable/disable menu handling
+            local player = GetPlayerPed(-1)
+            return IsPedOnFoot(player)
+        end,
+        data = {                                    -- Data that is passed to Javascript
+            keybind = "F2",                         -- Wheel keybind to use (case sensitive, must match entry in keybindControls table)
+            style = {                               -- Wheel style settings
+                sizePx = 600,                       -- Wheel size in pixels
+                slices = {                          -- Slice style settings
+                    default = { ['fill'] = '#000000', ['stroke'] = '#000000', ['stroke-width'] = 2, ['opacity'] = 0.60 },
+                    hover = { ['fill'] = '#ff8000', ['stroke'] = '#000000', ['stroke-width'] = 2, ['opacity'] = 0.80 },
+                    selected = { ['fill'] = '#ff8000', ['stroke'] = '#000000', ['stroke-width'] = 2, ['opacity'] = 0.80 }
+                },
+                titles = {                          -- Text style settings
+                    default = { ['fill'] = '#ffffff', ['stroke'] = 'none', ['font'] = 'Helvetica', ['font-size'] = 16, ['font-weight'] = 'bold' },
+                    hover = { ['fill'] = '#ffffff', ['stroke'] = 'none', ['font'] = 'Helvetica', ['font-size'] = 16, ['font-weight'] = 'bold' },
+                    selected = { ['fill'] = '#ffffff', ['stroke'] = 'none', ['font'] = 'Helvetica', ['font-size'] = 16, ['font-weight'] = 'bold' }
+                },
+                icons = {
+                    width = 64,
+                    height = 64
+                }
+            },
+            wheels = {                              -- Array of wheels to display
+                {
+                    navAngle = 270,                 -- Oritentation of wheel
+                    minRadiusPercent = 0.3,         -- Minimum radius of wheel in percentage
+                    maxRadiusPercent = 0.6,         -- Maximum radius of wheel in percentage
+                    labels = {"CANCEL", "NO", "CHEER", "CLAP", "FOLDARMS", "LEAN"},
+                    commands = {"e cancel", "e no", "e cheer", "e slowclap", "e foldarms", "e leanwall"}
+                },
+                {
+                    navAngle = 285,                 -- Oritentation of wheel
+                    minRadiusPercent = 0.6,         -- Minimum radius of wheel in percentage
+                    maxRadiusPercent = 0.9,         -- Maximum radius of wheel in percentage
+                    labels = {"SALUTE", "FINGER", "PEACE", "FACEPALM", "DAMN", "FAIL", "DEAD", "GANG1", "GANG2", "COP", "HOLSTER", "CROWDS"},
+                    commands = {"e salute", "e finger", "e peace", "e palm", "e damn", "e fail", "e dead", "e gang1", "e gang2", "e copidle", "e holster", "e copcrowd2"}
+                }
+            }
+        }
+    },
+    ['personal'] = {                                  -- Example menu for emotes when player is on foot
+        enableMenu = function()                     -- Function to enable/disable menu handling
+            local player = GetPlayerPed(-1)
+            return IsPedOnFoot(player)
+        end,
+        data = {                                    -- Data that is passed to Javascript
+            keybind = "F1",                         -- Wheel keybind to use (case sensitive, must match entry in keybindControls table)
+            style = {                               -- Wheel style settings
+                sizePx = 600,                       -- Wheel size in pixels
+                slices = {                          -- Slice style settings
+                    default = { ['fill'] = '#000000', ['stroke'] = '#000000', ['stroke-width'] = 2, ['opacity'] = 0.60 },
+                    hover = { ['fill'] = '#ff8000', ['stroke'] = '#000000', ['stroke-width'] = 2, ['opacity'] = 0.80 },
+                    selected = { ['fill'] = '#ff8000', ['stroke'] = '#000000', ['stroke-width'] = 2, ['opacity'] = 0.80 }
+                },
+                titles = {                          -- Text style settings
+                    default = { ['fill'] = '#ffffff', ['stroke'] = 'none', ['font'] = 'Helvetica', ['font-size'] = 16, ['font-weight'] = 'bold' },
+                    hover = { ['fill'] = '#ffffff', ['stroke'] = 'none', ['font'] = 'Helvetica', ['font-size'] = 16, ['font-weight'] = 'bold' },
+                    selected = { ['fill'] = '#ffffff', ['stroke'] = 'none', ['font'] = 'Helvetica', ['font-size'] = 16, ['font-weight'] = 'bold' }
+                },
+                icons = {
+                    width = 64,
+                    height = 64
+                }
+            },
+            wheels = {                              -- Array of wheels to display
+                {
+                    navAngle = 270,                 -- Oritentation of wheel
+                    minRadiusPercent = 0.3,         -- Minimum radius of wheel in percentage
+                    maxRadiusPercent = 0.6,         -- Maximum radius of wheel in percentage
+                    labels = {"imgsrc:smartphone.png", "imgsrc:backpack.png", "imgsrc:barbielyftupp.png", "imgsrc:remotekey.png", "imgsrc:closet.png"},
+                    commands = {"phone", "backpack", "barbielyftupp", "togglelock", "closetoutfit"}
+                },
+                --[[{
+                    navAngle = 285,                 -- Oritentation of wheel
+                    minRadiusPercent = 0.6,         -- Minimum radius of wheel in percentage
+                    maxRadiusPercent = 0.9,         -- Maximum radius of wheel in percentage
+                    labels = {},
+                    commands = {}
+                }--]]
+            }
+        }
+    },
+    ['vehicles'] = {                                -- Example menu for vehicle controls when player is in a vehicle
+        enableMenu = function()                     -- Function to enable/disable menu handling
+            local player = GetPlayerPed(-1)
+            return IsPedInAnyVehicle(player, false)
+        end,
+        data = {                                    -- Data that is passed to Javascript
+            keybind = "F1",                         -- Wheel keybind to use (case sensitive, must match entry in keybindControls table)
+            style = {                               -- Wheel style settings
+                sizePx = 400,                       -- Wheel size in pixels
+                slices = {                          -- Slice style settings
+                    default = { ['fill'] = '#000000', ['stroke'] = '#000000', ['stroke-width'] = 3, ['opacity'] = 0.60 },
+                    hover = { ['fill'] = '#ff8000', ['stroke'] = '#000000', ['stroke-width'] = 3, ['opacity'] = 0.80 },
+                    selected = { ['fill'] = '#ff8000', ['stroke'] = '#000000', ['stroke-width'] = 3, ['opacity'] = 0.80 }
+                },
+                titles = {                          -- Text style settings
+                    default = { ['fill'] = '#ffffff', ['stroke'] = 'none', ['font'] = 'Helvetica', ['font-size'] = 16, ['font-weight'] = 'bold' },
+                    hover = { ['fill'] = '#ffffff', ['stroke'] = 'none', ['font'] = 'Helvetica', ['font-size'] = 16, ['font-weight'] = 'bold' },
+                    selected = { ['fill'] = '#ffffff', ['stroke'] = 'none', ['font'] = 'Helvetica', ['font-size'] = 16, ['font-weight'] = 'bold' }
+                },
+                icons = {
+                    width = 64,
+                    height = 64
+                }
+            },
+            wheels = {                              -- Array of wheels to display
+                {
+                    navAngle = 270,                 -- Oritentation of wheel
+                    minRadiusPercent = 0.4,         -- Minimum radius of wheel in percentage
+                    maxRadiusPercent = 0.9,         -- Maximum radius of wheel in percentage
+                    labels = {"imgsrc:smartphone.png", "imgsrc:backpack.png", "imgsrc:engine.png", "imgsrc:padlock.png", "imgsrc:seatbelt.png", "imgsrc:doors.png", "imgsrc:trunk.png", "imgsrc:hood.png"},
+                    commands = {"phone", "backpack", "engine", "togglelock", "seatbelt", "rdoors", "trunk", "hood"}
+                }
+            }
+        }
+    }
+}
+
+-- Submenu configuration
+subMenuConfigs = {
+    ['closetoutfit'] = {
+        data = {
+            keybind = "F1",
+            style = {                               -- Wheel style settings
+                sizePx = 600,                       -- Wheel size in pixels
+                slices = {                          -- Slice style settings
+                    default = { ['fill'] = '#000000', ['stroke'] = '#000000', ['stroke-width'] = 2, ['opacity'] = 0.60 },
+                    hover = { ['fill'] = '#ff8000', ['stroke'] = '#000000', ['stroke-width'] = 2, ['opacity'] = 0.80 },
+                    selected = { ['fill'] = '#ff8000', ['stroke'] = '#000000', ['stroke-width'] = 2, ['opacity'] = 0.80 }
+                },
+                titles = {                          -- Text style settings
+                    default = { ['fill'] = '#ffffff', ['stroke'] = 'none', ['font'] = 'Helvetica', ['font-size'] = 16, ['font-weight'] = 'bold' },
+                    hover = { ['fill'] = '#ffffff', ['stroke'] = 'none', ['font'] = 'Helvetica', ['font-size'] = 16, ['font-weight'] = 'bold' },
+                    selected = { ['fill'] = '#ffffff', ['stroke'] = 'none', ['font'] = 'Helvetica', ['font-size'] = 16, ['font-weight'] = 'bold' }
+                },
+                icons = {
+                    width = 64,
+                    height = 64
+                }
+            },
+            wheels = {
+                {
+                    navAngle = 270,                 -- Oritentation of wheel
+                    minRadiusPercent = 0.3,         -- Minimum radius of wheel in percentage
+                    maxRadiusPercent = 0.6,         -- Maximum radius of wheel in percentage
+                    labels = {
+                      "imgsrc:smartphone.png", "imgsrc:backpack.png", "imgsrc:barbielyftupp.png", "imgsrc:remotekey.png", "imgsrc:closet.png"
+                       -- "imgsrc:car.png", "imgsrc:trunk.png", "imgsrc:key.png"
+                     },
+                    commands = {"togglephone", "toggleinventoryhud", "barbielyftupp", "vehicledoorlock", "closetoutfit"
+                     -- "test", "toggleinventoryhudtrunk", "doorvehiclelock"
+                   }
+                },
+                {
+                    navAngle = 270,
+                    minRadiusPercent = 0.7,
+                    maxRadiusPercent = 1.0,
+                    labels = {"imgsrc:jacket.png", "imgsrc:clothes.png", "imgsrc:sneaker.png", "imgsrc:theatre.png", "imgsrc:glasses.png", "imgsrc:earrings.png", "imgsrc:refresh.png", "imgsrc:helmet.png"},
+                    commands = {"jacket", "pants", "shoe", "mask", "glasses", "earrings", "ubie", "helmet"}
+                }
+            }
+        }
+    },
+}
